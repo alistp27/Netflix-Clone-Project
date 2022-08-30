fd30d573193e080dd16bdb16ef8826ebda4ce58d
ali sartipi <105682168+alistp27@users.noreply.github.com>
2022-08-30 16:55:59 +0430

    Update use-content.js

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -20,7 +20,7 @@ export default function useContent(target) {
             .catch((error) => {
                 console.log(error.message);
             });
-    }, []);
+    });
 
     return { [target]: Content };
 }
